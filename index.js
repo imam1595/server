b9727507e8ae8c931fac3815d125c5c02d853578
imam4747 <imam.hossain.f.edu.p@gmail.com>
2026-05-19 01:18:48 -0400

    post data and get data from server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require("cors")
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 dotenv.config();
@@ -9,6 +10,9 @@ const uri = process.env.MONGO_DB;
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -21,12 +25,36 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const db = client.db("driveEasy");
+    const carCollection = db.collection('cars');
+
+
+    app.get('/car', async (req, res) => {
+      const result = await carCollection.find().toArray();
+      res.json(result);
+    })
+
+    app.post('/car', async (req, res) => {
+      const carData = req.body;
+
+      console.log(carData);
+
+      const result = await carCollection.insertOne(carData);
+
+      res.json(result);
+    })
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
